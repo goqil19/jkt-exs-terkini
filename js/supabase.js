d480500210e097c42efe749224eaf699489a6691
goqil19 <goqil19@gmail.com>
2026-06-25 10:55:29 +0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,7 @@
 // CONFIGURATION
 // Ganti dengan URL dan Anon Key dari dashboard Supabase Anda
-const SUPABASE_URL = 'YOUR_SUPABASE_URL';
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY';
+const SUPABASE_URL = 'https://xpzebprkmnridfoyuswx.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_ztFxruwCCQE_hlHNbBD-kQ_wyH-TGYB';
 
 // Inisialisasi Supabase client
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
